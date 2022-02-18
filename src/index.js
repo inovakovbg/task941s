3938519bf6003d670e9396de3faa96e64e3027f7
Simona Mircheva <simona.mircheva@gmail.com>
2022-02-18 15:58:09 +0200

    fix: fix syntax, import still not loading

--- src/index.js
+++ src/index.js
@@ -9,3 +9,26 @@ window.addEventListener("DOMContentLoaded", () => {
   app.addBananas();
   window.__JS_APP = app;
 });
+
+
+// function constructor() {
+//   this.emojis = [];
+//   this.banana = "🍌";
+// }
+
+// const numbers = ["🐒", "🦍", "🦧"];
+// const newArr = numbers.map(myFunction)
+
+// function myFunction(num) {
+//   return num + "🍌";
+// }
+
+// let fLen = newArr.length;
+// let order = "<p>";
+// for (let i = 0; i < fLen; i++) {
+//   order += "<p>" + newArr[i] + "</p>";
+// }
+// order += "</p>";
+
+// document.getElementById("emojiss").innerHTML = "🐒" + "🍌" + "🦍"+ "🍌"+ "🦧"+"🍌";
+// document.getElementById("emojis").innerHTML = order;
